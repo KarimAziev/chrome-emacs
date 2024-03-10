1a48be8b86d534e9f340126bca79e11f0512384f
KarimAziev <karim.aziiev@gmail.com>
2024-03-10 11:18:24 +0200

    feat: enhance CodeMirror5 handler with focus and selection features
    This commit introduces improvements to the InjectedCodeMirror5Handler,
    focusing on enhancing user interaction with the editor. Key features
    added include automatically focusing the editor upon setting its value
    and managing cursor visibility for better user feedback. Additionally,
    a new private method, setSelection, has been implemented to handle
    multiple selections more effectively, providing a more intuitive and
    responsive editing experience.

--- src/handlers/injected/codemirror5.ts
+++ src/handlers/injected/codemirror5.ts
@@ -33,15 +33,47 @@ class InjectedCodeMirror5Handler extends BaseInjectedHandler<HTMLDivElement> {
   setValue(text: string, options?: UpdateTextPayload): void {
     this.executeSilenced(() => {
       this.editor.setValue(text);
-      const position = isNumber(options?.lineNumber) &&
-        isNumber(options?.column) && {
-          line: options.lineNumber - 1,
-          ch: options.column - 1,
-        };
-      if (position) {
-        this.editor?.setCursor(position, undefined, { scroll: true });
-      }
+      this.editor?.focus();
+      this.setPosition(options);
+      this.setSelection(options?.selections);
     });
+  }
+
+  private setPosition(options?: UpdateTextPayload) {
+    if (isNumber(options?.lineNumber) && isNumber(options.column)) {
+      const position = {
+        line: options.lineNumber - 1,
+        ch: options.column - 1,
+      };
+
+      this.editor?.setCursor(position, undefined, { scroll: true });
+
+      const cursorEl = this.elem.querySelector<HTMLElement>(
+        '.CodeMirror-cursors',
+      );
+
+      if (cursorEl) {
+        cursorEl.style.visibility = 'visible';
+      }
+    }
+  }
+
+  private setSelection(selections?: UpdateTextPayload['selections']) {
+    const sels = selections?.map(({ start, end }) => {
+      const posA = this.editor.posFromIndex(start);
+      const posB = this.editor.posFromIndex(end);
+      return { anchor: posB, head: posA };
+    });
+    if (sels && sels.length > 1) {
+      if (sels) {
+        this.editor.setSelections(sels, 0);
+      }
+    } else if (sels) {
+      const sel = sels[0];
+      if (sel) {
+        this.editor.setSelection(sel.head, sel.anchor);
+      }
+    }
   }
 
   bindChange(f: () => void): void {
