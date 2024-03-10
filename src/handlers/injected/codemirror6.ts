01dfbef108c6916cc9e4bb600da604a896ced43c
KarimAziev <karim.aziiev@gmail.com>
2024-03-10 15:29:35 +0200

    feat(codemirror6.ts): sync position and mode detection in codemirror6
    - Refactor `InjectedCodeMirror6Handler` to use `CMContentElement` interface for better type safety.
    - Implement `getSelection` method to handle text selection upon setting value.
    - Add `showCursor` method to ensure cursor visibility.
    - Introduce `getPosition` method to fetch the current cursor position.
    - Modify `getVisualElement` to target `.cm-editor` for visual element retrieval.
    - Implement `getExtension` method to determine file extension based on language mode.
    - Import necessary utilities and configurations for extended functionality.

--- src/handlers/injected/codemirror6.ts
+++ src/handlers/injected/codemirror6.ts
@@ -1,14 +1,23 @@
 import BaseInjectedHandler from '@/handlers/injected/base';
 import { UpdateTextPayload } from '@/handlers/types';
+import { fileExtensionsByLanguage } from '@/handlers/config/codemirror';
+import 'codemirror/mode/meta';
+import DummyCodeMirror from 'dummy-codemirror';
+import { isNumber } from '@/util/guard';
 
 export type EditorView = import('@codemirror/view').EditorView;
 
-class InjectedCodeMirror6Handler extends BaseInjectedHandler<HTMLDivElement> {
+interface CMContentElement extends HTMLDivElement {
+  cmView: {
+    view: EditorView;
+  };
+}
+
+class InjectedCodeMirror6Handler extends BaseInjectedHandler<CMContentElement> {
   editor!: EditorView;
 
   async load(): Promise<void> {
-    this.editor = (this.elem.closest('.cm-content') as any).cmView
-      .view as EditorView;
+    this.editor = this.elem.cmView.view;
   }
   getValue(): string {
     const fieldValue = this.editor.state.doc.toString();
@@ -16,7 +25,8 @@ class InjectedCodeMirror6Handler extends BaseInjectedHandler<HTMLDivElement> {
     return fieldValue;
   }
 
-  setValue(text: string, _options?: UpdateTextPayload): void {
+  setValue(text: string, options?: UpdateTextPayload): void {
+    const selection = this.getSelection(options);
     this.editor.dispatch({
       changes: {
         from: 0,
@@ -24,13 +34,63 @@ class InjectedCodeMirror6Handler extends BaseInjectedHandler<HTMLDivElement> {
         insert: text,
       },
     });
+
+    if (selection) {
+      this.editor.dispatch({
+        selection,
+        userEvent: 'select',
+        scrollIntoView: true,
+      });
+    }
+    this.showCursor();
+  }
+
+  private getSelection(options?: UpdateTextPayload) {
+    const selections = options?.selections?.map(({ start, end }) => ({
+      anchor: end,
+      head: start,
+    }));
+
+    const selection =
+      (selections && selections[0]) ||
+      (isNumber(options?.lineNumber) &&
+        isNumber(options.column) && {
+          anchor:
+            this.editor?.state?.doc?.line(options.lineNumber).from +
+            options.column -
+            1,
+        });
+    return selection || undefined;
+  }
+
+  showCursor() {
+    const visual = this.getVisualElement();
+
+    if (visual) {
+      const isFocused = visual.classList.contains('cm-focused');
+
+      if (!isFocused) {
+        visual.classList.add('cm-focused');
+      }
+    }
+  }
+
+  getPosition() {
+    try {
+      const offset = this.editor.state.selection.main.head;
+      const line = this.editor.state.doc.lineAt(offset);
+
+      return { lineNumber: line.number, column: offset - line.from + 1 };
+    } catch (error) {
+      return {
+        lineNumber: 1,
+        column: 1,
+      };
+    }
   }
 
   getVisualElement() {
-    const cm6 = this.elem.closest('.cm-content');
-    if (cm6) {
-      return cm6;
-    }
+    return this.elem.closest<HTMLDivElement>('.cm-editor');
   }
 
   bindChange(f: () => void): void {
@@ -39,6 +99,25 @@ class InjectedCodeMirror6Handler extends BaseInjectedHandler<HTMLDivElement> {
 
   unbindChange(f: () => void): void {
     this.editor.dom.removeEventListener('input', f);
+  }
+  getExtension(): string | null {
+    const currentModeName = this.elem.dataset.language;
+
+    // we use some hardcoded overrides because the `modeInfo` may contain duplicates, e.g., css => gcss
+    if (currentModeName && fileExtensionsByLanguage[currentModeName]) {
+      return fileExtensionsByLanguage[currentModeName];
+    }
+
+    if (currentModeName && DummyCodeMirror.modeInfo) {
+      for (const mode of DummyCodeMirror.modeInfo) {
+        if (mode.mode === currentModeName && mode.ext) {
+          const extension = mode.ext[0];
+
+          return extension;
+        }
+      }
+    }
+    return null;
   }
 }
 
