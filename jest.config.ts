20006fdc6d249d6974498a08cdab0a3239b24586
KarimAziev <karim.aziiev@gmail.com>
2024-08-17 10:06:06 +0300

    test: update jest config to collect coverage from src files

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,11 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: './jest.environment.ts',
-
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+  ],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
